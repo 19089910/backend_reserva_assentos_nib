348b9b946f7cbc3635a5a4947acd02f5ce96dcb1
Lucas Barbosa <lucasbarbosaferreira.lb@gmail.com>
2024-08-26 23:22:30 -0400

    Add user creation with validation and existence check - Added Yup validation schema to validate user input, including name, email, password, and admin fields. - Implemented check for existing users to prevent duplicate email addresses. - Improved error handling and validation error messages for clearer feedback.

--- src/app/controllers/UserController.js
+++ src/app/controllers/UserController.js
@@ -1,10 +1,37 @@
 import { v4 } from 'uuid'
 import User from '../schemas/User'
+import * as Yup from 'yup'
 
 class UserController {
   async store(request, response) {
-    // perga via body do front
+    const schema = Yup.object().shape({
+      name: Yup.string().required('Name is required'),
+      email: Yup.string()
+        .email('Invalid email format')
+        .required('Email is required'),
+      password: Yup.string()
+        .required('Password is required')
+        .min(4, 'Password must be at least 4 characters'),
+      admin: Yup.boolean(),
+    })
+    try {
+      await schema.validateSync(request.body, { abortEarly: false })
+    } catch (err) {
+      const validationErrors = err.errors || err.inner.map((e) => e.message)
+      return response.status(400).json({ error: validationErrors })
+    }
+
     const { name, email, password, admin } = request.body
+
+    // Verificar se o usuário já existe
+    const userExists = await User.findOne({
+      where: { email },
+    })
+
+    if (userExists) {
+      return response.status(409).json({ error: 'User already existe' })
+      // perga via body do front
+    }
 
     // manda para o model User e ainda armazena em variavel user
     const user = await User.create({
